f4bd30ca30c3dcf419bac1f6188ce2e085e5acf2
eojin <eozinee@gmail.com>
2020-06-27 23:33:29 +0900

    Add Folders

---
+++ Influencer/Main/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Influencer
+//
+//  Created by 양어진 on 2020/06/26.
+//  Copyright © 2020 eojine. All rights reserved.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    @IBOutlet private weak var searchButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
